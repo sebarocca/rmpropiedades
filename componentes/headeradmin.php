55d54e9f3ec7ba5f7f2ef9b827537bfbea4546a2
Sebastian Rocca <roccabsebastian@gmail.com>
2020-11-19 19:47:09 -0300

    ultimo cambio

--- componentes/headeradmin.php
+++ componentes/headeradmin.php
@@ -20,6 +20,7 @@ header("Location: login.php");
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
